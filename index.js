3b6602811c6507111fea1319bb9a24f516af6cb8
Khaled M <k.code@icloud.com>
2019-01-08 17:47:00 -0500

    + models/index.js+todo.js + routes/todos.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require("express"),
     app     = express();
 
+var todoRoutes = require("./routes/todos");
+
+app.use("/api/todos", todoRoutes);
 
 app.get("/", function(req, res) {
-    res.send("Hi");
+    res.send("Hi, from root route");
 });
-
 
 app.listen(8000, function() {
     console.log("Server running");
